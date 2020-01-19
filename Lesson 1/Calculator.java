1139a325d25a7554441190224cef3482b4bb30a9
gsold2 <gsold2@gmail.com>
2020-01-19 15:05:25 +0300

    убрал лишнее на вводе в строку

--- Lesson 1/Calculator.java
+++ Lesson 1/Calculator.java
@@ -5,12 +5,12 @@ public class Calculator {
 		//TODO: калькулятор для командной строки
 		System.out.println("Возможные типы математических операций " +
 							"с целыми положительными числами:" + "\n" +
-							"Сложение - введите с клавиатуры +" + "\n" +
-							"Вычетание - введите с клавиатуры -" + "\n" +
-							"Умножение - введите с клавиатуры *" + "\n" +
-							"Деление - введите с клавиатуры /" + "\n" +
-							"Деление по модулю - введите с клавиатуры %" + "\n" +
-							"Возведение в степень - введите с клавиатуры ^"  + "\n");
+							"[Первое число]+[Второе число] введите с клавиатуры +" + "\n" +
+							"[Первое число]-[Второе число] введите с клавиатуры -" + "\n" +
+							"[Первое число]*[Второе число] введите с клавиатуры *" + "\n" +
+							"[Первое число]/[Второе число] введите с клавиатуры /" + "\n" +
+							"[Первое число]%[Второе число] введите с клавиатуры %" + "\n" +
+							"[Первое число]^[Второе число] введите с клавиатуры ^"  + "\n");
 		System.out.println("Введите симовол операции из предложенных выше:");
 
 		Scanner in = new Scanner(System.in);
@@ -32,58 +32,32 @@ public class Calculator {
 			}
 		}
 
-		if(inputMathSymbole == '+') {
-			System.out.println("Введите первое слогаемое:");
-		} else if(inputMathSymbole == '-') {
-			System.out.println("Введите уменьшаемое:");
-		} else if(inputMathSymbole == '*') {
-			System.out.println("Введите первый множитель:");
-		} else if(inputMathSymbole == '/') {
-			System.out.println("Введите делимое:");
-		} else if(inputMathSymbole == '%') {
-			System.out.println("Введите делимое:");
-		} else if(inputMathSymbole == '^') {
-			System.out.println("Введите основание:");
-		}
-
+		System.out.println("Введите [Первое число]:");
 		int number1 = inputNumber();
 
-		if(inputMathSymbole == '+') {
-			System.out.println("Введите второе слогаемое:");
-		} else if(inputMathSymbole == '-') {
-			System.out.println("Введите вычитаемое:");
-		} else if(inputMathSymbole == '*') {
-			System.out.println("Введите второй множитель:");
-		} else if(inputMathSymbole == '/') {
-			System.out.println("Введите делитель:");
-		} else if(inputMathSymbole == '%') {
-			System.out.println("Введите делитель:");
-		} else {
-			System.out.println("Введите степень:");
-		}
-
+		System.out.println("Введите [Второе число]:");
 		int number2 = inputNumber();
 
 		if(inputMathSymbole == '+') {
-			System.out.println("Сумма слогаемых равна " + (number1 + number2));
+			System.out.println(number1 + "+" + number2 + "=" + (number1 + number2));
 		} else if(inputMathSymbole == '-') {
-			System.out.println("Разность равна " + (number1 - number2));
+			System.out.println(number1 + "-" + number2 +  "=" + (number1 - number2));
 		} else if(inputMathSymbole == '*') {
-			System.out.println("Произведение равно " + (number1 * number2));
+			System.out.println(number1 + "*" + number2 +  "=" + (number1 * number2));
 		} else if(inputMathSymbole == '/') {
-			System.out.println("Частное равно " + (number1 / number2));
+			System.out.println(number1 + "/" + number2 +  "=" + (number1 / number2));
 		} else if(inputMathSymbole == '%') {
-			System.out.println("Остаток деления " + (number1 % number2));
+			System.out.println(number1 + "%" + number2 +  "=" + (number1 % number2));
 		} else {
 			int exp = number1;
 			for (int i = 1; i < number2; i++) {
 				exp *= number1;
 			}
-			System.out.println("Результат возведения в степень " + exp);
+			System.out.println(number1 + "^" + number2 + "=" + exp);
 		}
 	}
 
-	private static int inputNumber() {
+	public static int inputNumber() {
 		int number = 0;
 		boolean isNumber = false;
 		Scanner in = new Scanner(System.in);
