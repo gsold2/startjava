1d9f38c68ebb0dd108ba8139a375976002616d0d
gsold2 <gsold2@gmail.com>
2020-01-21 18:39:05 +0300

    Добавил Person

---
+++ Lesson 2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+	String sex = "Man";
+	String name =  "Gregory";
+	float height = 1.8f;
+	float waight = 89f;
+	int age = 44;
+
+	void go() {
+		System.out.println("Я ищу");
+	}
+
+	void sit() {
+		System.out.println("Я сижу");
+	}
+
+	void run() {
+		System.out.println("Я бегу");
+	}
+
+	String speak() {
+		return "Я говорю"
+	}
+
+	void learnJava() {
+		System.out.println("Я изучаю Java");
+	}
+}
