785936dc557897cb87cc5673ccd4c027ea918f3a
gsold2 <gsold2@gmail.com>
2020-01-26 16:45:01 +0300

    Сделал Егеря и ЕгерьТест с геттерма и сеттерами и конструктором

---
+++ Lesson 2/Jaeger.java
@@ -0,0 +1,132 @@
+public class Jaeger {
+	int launchedYear;
+	String mark;
+	String origin;
+	boolean status;
+	float height;
+	float weight;
+	int speed;
+	int strength;
+	int armor;
+	String energyCore;
+	String bodyLanguage;
+	int kajuKillded;
+
+	public Jaeger(int launchedYear, String mark, String origin, boolean status,
+				float height, float weight, int speed, int strength, int armor,
+				String energyCore, String bodyLanguage, int kajuKillded) {
+		this.launchedYear = launchedYear;
+		this.mark = mark;
+		this.origin = origin;
+		this.status = status;
+		this.height = height;
+		this.weight = weight;
+		this.speed = speed;
+		this.strength = strength;
+		this.armor = armor;
+		this.energyCore = energyCore;
+		this.bodyLanguage = bodyLanguage;
+		this.kajuKillded = kajuKillded;
+	}
+
+	public void setLaunchedYear(int launchedYear) {
+		this.launchedYear = launchedYear;
+	}
+	public int getlaunchedYear() {
+		return launchedYear;
+	}
+
+	public void setMark(String mark) {
+		this.mark = mark;
+	}
+	public String getMark() {
+		return mark;
+	}
+
+	public void setOrigin(String origin) {
+		this.origin = origin;
+	}
+	public String getOrigin() {
+		return origin;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+	public boolean getStatus() {
+		return status;
+	}
+
+	public void setHeight(float height) {
+		this.height = height;
+	}
+	public float getHeight() {
+		return height;
+	}
+
+	public void setWeight(float weight) {
+		this.weight = weight;
+	}
+	public float getWeight() {
+		return weight;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setStrength(int strength) {
+		this.strength = strength;
+	}
+	public int getStrength() {
+		return strength;
+	}
+
+	public void setArmor(int armor) {
+		this.armor = armor;
+	}
+	public int getArmor() {
+		return armor;
+	}
+
+	public void setEnergyCore(String energyCore) {
+		this.energyCore = energyCore;
+	}
+	public String getEnergyCore() {
+		return energyCore;
+	}
+
+	public void setBodyLanguage(String bodyLanguage) {
+		this.bodyLanguage = bodyLanguage;
+	}
+	public String getBodyLanguage() {
+		return bodyLanguage;
+	}
+
+	public void setKajuKillded(int kajuKillded) {
+		this.kajuKillded = kajuKillded;
+	}
+	public int getKajuKillded() {
+		return kajuKillded;
+	}
+
+	public void status() {
+		System.out.print(
+		"launchedYear = " + launchedYear + "\n" +
+		"mark = " + mark + "\n" +
+		"origin = " + origin + "\n" +
+		"status = " + status + "\n" +
+		"height = " + height + "\n" +
+		"weight = " + weight + "\n" +
+		"speed = " + speed + "\n" +
+		"strength = " + strength + "\n" +
+		"armor = " + armor + "\n" +
+		"energyCore = " + energyCore + "\n" +
+		"bodyLanguage = " + bodyLanguage + "\n" +
+		"kajuKillded = " + kajuKillded + "\n"
+		);
+	}
+}
---
+++ Lesson 2/JaegerTest.java
@@ -0,0 +1,46 @@
+public class JaegerTest {
+	public static void main(String[] args) {
+		//TODO создаем двух егерей ссылни на описание ниже
+		//https://pacificrim.fandom.com/wiki/Striker_Eureka_(Jaeger)
+		//https://pacificrim.fandom.com/wiki/Cherno_Alpha_(Jaeger)
+		Jaeger strikerEureka = new Jaeger(
+			2019,
+			"Mark-5",
+			"Australia",
+			false,
+			76.2f,
+			1.85f,
+			10,
+			10,
+			9,
+			"XIG Supercell Chamber",
+			"Dirty Boxer",
+			10
+		);
+		strikerEureka.status();
+
+		Jaeger chernoAlpha = new Jaeger(
+			2015,
+			"Mark-1",
+			"Russia",
+			false,
+			85.34f,
+			2.412f,
+			3,
+			10,
+			9,
+			"Stun Core 08",
+			"DGreco-Roman wrestling/boxing",
+			8
+		);
+		chernoAlpha.status();
+
+		System.out.println(chernoAlpha.getMark() + " убил " + chernoAlpha.getKajuKillded());
+		chernoAlpha.setKajuKillded(9);
+		System.out.println(chernoAlpha.getMark() + " убил " + chernoAlpha.getKajuKillded());
+
+		System.out.println(chernoAlpha.getMark() + " создан в " + chernoAlpha.getOrigin());
+		chernoAlpha.setOrigin("Лукоморье");
+		System.out.println(chernoAlpha.getMark() + " создан в " + chernoAlpha.getOrigin());
+	}
+}
